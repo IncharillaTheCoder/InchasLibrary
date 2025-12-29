1adb9b4af973a9534e9e76b67b968c145425296a
IncharillaTheCoder <188787722+IncharillaTheCoder@users.noreply.github.com>
2025-12-29 16:17:03 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -490,7 +490,7 @@ function IncharillaUI:addButton(tabName, buttonText, callback)
     end
     
     local button = Instance.new("TextButton")
-    button.Size = UDim2.new(1, -0, 0, 48)
+    button.Size = UDim2.new(1, 10, 0, 48)
     button.BackgroundColor3 = Themes[self.currentTheme].button
     button.BackgroundTransparency = 0.1
     button.BorderSizePixel = 0
